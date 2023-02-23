2e727f7800d22d79b48497e982ba5c63842f2e4e
Diego Chagas <diego.chagas@afya.com.br>
2023-02-23 10:20:21 -0300

    subindo piloto table especialidades

---
+++ src/App.jsx
@@ -0,0 +1,129 @@
+
+
+import './App.css'
+// import './teste.css'
+
+function App() {
+  
+  return (
+    <div className='container'>
+      <h2> Ranking de especialidades</h2>
+
+      <div className='teste'>
+
+     
+     
+       <table>
+       
+        <thead>
+          <tr>
+          <th rowSpan="1"></th>
+          <th colSpan="2">Especialidade</th>
+          <th>Acerto em questões</th>
+          <th>Playlists Concluídas</th>
+          <th>Questões realizadas</th>
+          <th>Acessar Playlist</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>1°</td>
+            <td></td>
+            <td>Cardiologia</td>
+            <td>0%</td>
+            <td>0</td>
+            <td>0</td>
+            <td><a href="">Ver Playlist</a></td>
+          </tr>
+          <tr>
+            <td>2°</td>
+            <td></td>
+            <td>Pediatria</td>
+            <td>0%</td>
+            <td>0</td>
+            <td>0</td>
+            <td><a href="">Ver Playlist</a></td>
+          </tr>
+          <tr>
+            <td>3°</td>
+            <td></td>
+            <td>Ginecologia <br></br> e Obstetricia</td>
+            <td>0%</td>
+            <td>0</td>
+            <td>0</td>
+            <td><a href="">Ver Playlist</a></td>
+          </tr>
+          <tr>
+            <td>4°</td>
+            <td></td>
+            <td>Anestesiologia</td>
+            <td>0%</td>
+            <td>0</td>
+            <td>0</td>
+            <td><a href="">Ver Playlist</a></td>
+          </tr>
+          <tr>
+            <td>5°</td>
+            <td></td>
+            <td>Nefrologia</td>
+            <td>0%</td>
+            <td>0</td>
+            <td>0</td>
+            <td><a href="">Ver Playlist</a></td>
+          </tr>
+          <tr>
+            <td>6°</td>
+            <td></td>
+            <td>Cirurgia Geral</td>
+            <td>0%</td>
+            <td>0</td>
+            <td>0</td>
+            <td><a href="">Ver Playlist</a></td>
+          </tr>
+          <tr>
+            <td>7°</td>
+            <td></td>
+            <td>Cardiologia</td>
+            <td>0%</td>
+            <td>0</td>
+            <td>0</td>
+            <td><a href="">Ver Playlist</a></td>
+          </tr>
+          <tr>
+            <td>8°</td>
+            <td></td>
+            <td>Cirurgia do <br></br> trauma</td>
+            <td>0%</td>
+            <td>0</td>
+            <td>0</td>
+            <td><a href="">Ver Playlist</a></td>
+          </tr>
+          <tr>
+            <td>9°</td>
+            <td></td>
+            <td>Oncologia</td>
+            <td>0%</td>
+            <td>0</td>
+            <td>0</td>
+            <td><a href="">Ver Playlist</a></td>
+          </tr>
+          <tr>
+            <td>10°</td>
+            <td></td>
+            <td>Neurocirurgia</td>
+            <td>0%</td>
+            <td>0</td>
+            <td>0</td>
+            <td><a href="">Ver Playlist</a></td>
+          </tr>
+        </tbody>
+       </table>
+       
+       
+</div>
+    </div>
+  )
+}
+
+export default App
